8afc15dcab16ff1c9ad6d77ea24181275ab6a4ec
Darren <nardol.darren@gmail.com>
2024-08-07 17:40:06 +0200

    add catway services

--- routes/catways.js
+++ routes/catways.js
@@ -2,10 +2,10 @@ const express = require("express");
 const router = express.Router();
 const service = require("../services/catways");
 
-//router.post("/", service.createOneCatway);
-//router.get("/", service.getAllCatways);
-//router.get("/:id", service.getOneCatway);
-//router.put("/:id", service.modifyOneCatway);
-//router.delete("/:id", service.deleteOneCatway);
+router.post("/", service.createCatway);
+router.get("/", service.getAllCatways);
+router.get("/:id", service.getOneCatway);
+router.put("/:id", service.updateCatway);
+router.delete("/:id", service.deleteCatway);
 
 module.exports = router;
