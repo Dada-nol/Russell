916f3a5c749a00801f0be8a61d60d01ef3623600
Darren <nardol.darren@gmail.com>
2024-08-10 17:20:21 +0200

    add authenticate service

--- services/users.js
+++ services/users.js
@@ -99,3 +99,43 @@ exports.delete = async (req, res, next) => {
     return res.status(501).json(error);
   }
 };
+
+exports.authenticate = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  try {
+    let user = await User.findOne({ email: email }, "-__v -createdAt");
+
+    if (user) {
+      bcrypt.compare(password, user.password, function (err, response) {
+        if (err) {
+          throw new Error(err);
+        }
+        if (response) {
+          delete user._doc.password;
+
+          const expireIn = 24 * 60 * 60;
+          const token = jwt.sign(
+            {
+              user: user,
+            },
+            process.env.SECRET_KEY,
+            {
+              expiresIn: expireIn,
+            }
+          );
+
+          res.header("Authorization", "Bearer" + token);
+
+          return res.status(200).json("authenticate_succeed");
+        }
+
+        return res.status(403).json("wrong_credentials");
+      });
+    } else {
+      return res.status(404).json("user_not_found");
+    }
+  } catch (error) {
+    return res.status(501).json(error);
+  }
+};
