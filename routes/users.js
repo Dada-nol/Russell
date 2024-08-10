f5ab3ccce205a9d69191cc818ba9dc13053ba31d
Darren <nardol.darren@gmail.com>
2024-08-10 17:08:25 +0200

    add forms for user_CRUD

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,44 @@
 const express = require("express");
 const router = express.Router();
 const service = require("../services/users");
+const User = require("../models/user");
+
+router.get("/add", async (req, res, next) => {
+  res.render("user/create_user", {
+    title: "Create",
+  });
+});
+
+router.get("/update/:id", async (req, res, next) => {
+  const id = req.params.id;
+  let user = await User.findById(id);
+  res.render("user/update_user", {
+    title: "Update",
+    user: user,
+  });
+});
+
+router.get("/delete/:id", async (req, res, next) => {
+  const id = req.params.id;
+  let user = await User.findById(id);
+  res.render("user/delete_user", {
+    title: "Delete",
+    user: user,
+  });
+});
+
+router.get("/read/:id", async (req, res, next) => {
+  const id = req.params.id;
+  let user = await User.findById(id);
+  res.render("user/read_user", {
+    title: "Read",
+    user: user,
+  });
+});
 
 router.get("/:id", service.getById);
 router.get("/", service.getAllUsers);
-router.put("/add", service.add);
+router.post("/", service.add);
 router.patch("/:id", service.update);
 router.delete("/:id", service.delete);
 
