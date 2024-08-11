02707fdfff18d5ac3cd96faa95d0f804cf0a2b96
Darren <nardol.darren@gmail.com>
2024-08-11 20:59:49 +0200

    add routes

--- routes/reservations.js
+++ routes/reservations.js
@@ -1,11 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const service = require("../services/reservations");
-//const private = require("../middlewares/private");
 
-//router.get("/:id", private.checkJWT, service.getById);
-//router.get("/", service.getAllReservations);
-//router.delete("/:idReservation", private.checkJWT, service.delete);
-//router.post("/", service.addReservation);
+router.get("/");
+router.get("/:idReservation");
+router.post("/");
+router.delete("/:idReservation");
 
 module.exports = router;
