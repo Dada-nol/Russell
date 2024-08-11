c7a000a880edf6b753be30e5aabf2709bb05f0bc
Darren <nardol.darren@gmail.com>
2024-08-11 20:56:44 +0200

    create model resrvation

---
+++ models/reservation.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const Reservation = new Schema({
+  catwayNumber: {
+    type: [Number],
+  },
+  clientName: {
+    type: [String],
+  },
+  boatName: {
+    type: String,
+    trim: true,
+    required: [true, "Le nom du bateau est requis"],
+  },
+  checkIn: {
+    type: Date,
+    trim: true,
+    required: [true, "La date de début de réservation est requise est requise"],
+  },
+  checkOut: {
+    type: Date,
+    trim: true,
+    required: [true, "La date de fin de réservation est requise est requise"],
+  },
+});
+
+module.exports = mongoose.model("Reservation", Reservation);
