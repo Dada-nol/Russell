0e7f87c870a0a59cd85efb64d24562335f73d308
Darren <nardol.darren@gmail.com>
2024-08-12 09:46:16 +0200

    add CRUD_reservation

--- routes/reservations.js
+++ routes/reservations.js
@@ -2,9 +2,9 @@ const express = require("express");
 const router = express.Router();
 const service = require("../services/reservations");
 
-router.get("/");
+router.get("/:id/reservations/", service.getAllReservations);
 router.get("/:id/reservations/:idReservation", service.getOneReservation);
 router.post("/:id/reservations/", service.add);
-router.delete("/:id");
+router.delete("/:id/reservations/:idReservation", service.deleteReservation);
 
 module.exports = router;
